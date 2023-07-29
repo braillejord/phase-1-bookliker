d32da0f6b105b93e372f868e53bfa236a31ec805
BreElle Wells <breellejordyn@gmail.com>
2023-07-29 13:11:21 -0600

    fetch and render book list

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-document.addEventListener("DOMContentLoaded", function() {});
+const bookUrl = 'http://localhost:3000/books'
+
+const bookList = document.getElementById('list')
+
+// fetch list of books
+fetch(bookUrl)
+.then(r => r.json())
+.then(data => renderBooks(data))
+
+// render book list
+function renderBooks(allBooks) {
+    allBooks.forEach( (singleBook) => {
+        const oneBook = document.createElement('li')
+        oneBook.innerText = singleBook.title
+
+        bookList.appendChild(oneBook)
+    })
+}   
